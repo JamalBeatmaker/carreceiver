49740a6280f58da317ba18603f0fdf579976b058
Hackids Education <65713501+HackidsEdu@users.noreply.github.com>
2021-07-22 20:32:54 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Abrir essa página em [https://hackidsedu.github.io/carreceiver/](https://hackidsedu.github.io/carreceiver/)
+
+## Usar como extensão
+
+Este repositório pode ser adicionado como **extensão** no MakeCode.
+
+* abrir [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* clique em **Novo Projeto**
+* clique em **Extensões** em baixo do menu com ícone de engrenagem
+* procure por **https://github.com/hackidsedu/carreceiver** e importe
+
+## Editar este projeto ![Ícone de estado da compilação](https://github.com/hackidsedu/carreceiver/workflows/MakeCode/badge.svg)
+
+Para editar este repositório no MakeCode.
+
+* abrir [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* clique em **Importar** e depois clique em **Importar URL**
+* cole **https://github.com/hackidsedu/carreceiver** e clique em importar
+
+## Pré-visualização de blocos
+
+"Essa imagem mostra o bloco de códigos da última confirmação no "mestre"".
+Esta imagem pode demorar alguns minutos para atualizar.
+
+![Uma visão renderizada dos blocos](https://github.com/hackidsedu/carreceiver/raw/master/.github/makecode/blocks.png)
+
+#### Metadados (usados para pesquisa, renderização)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,104 @@
+function right () {
+    hackbitmotors.MotorRunDual(
+    hackbitmotors.Motors.M1A,
+    30,
+    hackbitmotors.Motors.M2B,
+    150
+    )
+}
+function back () {
+    hackbitmotors.MotorRunDual(
+    hackbitmotors.Motors.M1A,
+    -150,
+    hackbitmotors.Motors.M2B,
+    -150
+    )
+}
+function front () {
+    hackbitmotors.MotorRunDual(
+    hackbitmotors.Motors.M1A,
+    150,
+    hackbitmotors.Motors.M2B,
+    150
+    )
+}
+function left () {
+    hackbitmotors.MotorRunDual(
+    hackbitmotors.Motors.M1A,
+    150,
+    hackbitmotors.Motors.M2B,
+    30
+    )
+}
+radio.onReceivedValue(function (name, value) {
+    if (name == "U") {
+        vU = value
+        if (vU == 0) {
+            front()
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
+        }
+    } else if (name == "D") {
+        vD = value
+        if (vD == 0) {
+            back()
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                `)
+        }
+    } else if (name == "L") {
+        vL = value
+        if (vL == 0) {
+            left()
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+        }
+    } else if (name == "R") {
+        vR = value
+        if (vR == 0) {
+            right()
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+        }
+    } else if (name == "M") {
+        vM = value
+        if (vM == 0) {
+            hackbitmotors.MotorStopAll()
+            basic.showLeds(`
+                . . # . .
+                . # . # .
+                # . . . #
+                . # . # .
+                . . # . .
+                `)
+        }
+    }
+})
+let vM = 0
+let vR = 0
+let vL = 0
+let vD = 0
+let vU = 0
+radio.setGroup(101)
+radio.setTransmitPower(6)
+led.enable(true)
+basic.clearScreen()
